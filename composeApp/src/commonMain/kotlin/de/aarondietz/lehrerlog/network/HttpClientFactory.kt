aa7f748642424b28a8535bac36a36569f2125ab8
Aaron Dietz <info@aarondietz.de>
2026-01-10 17:08:44 +0100

    Adds missed file

---
+++ composeApp/src/commonMain/kotlin/de/aarondietz/lehrerlog/network/HttpClientFactory.kt
@@ -0,0 +1,25 @@
+package de.aarondietz.lehrerlog.network
+
+import de.aarondietz.lehrerlog.SERVER_PORT
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+fun createHttpClient(): HttpClient {
+    return HttpClient {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+        defaultRequest {
+            contentType(ContentType.Application.Json)
+        }
+    }
+}
